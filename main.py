948b953562cbad150fe6a3366f463527da9f6b33
AlessandroCanel <58002238+AlessandroCanel@users.noreply.github.com>
2020-10-07 17:20:19 +0200

    Tried to do the Alphabetical to raw binary but failed, i need to move along the for but do not know how

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import re
 
+
+def binary(one, two):
+    for y in one:
+        if y == 'Z':
+            two = two + '0'
+        elif y == 'O':
+            two = two + '1'
+    return two
+
+
 with open('challenge.txt', 'r') as file:
     phrase = file.read().replace('\n', '')
 
 result = re.sub('[^A-Z]', '', phrase)
 
-print(result, file = open("output.txt", "a"))
+x = input("Would you like to binary? y/n  ")
+
+if x == 'y':
+    AlternativeOutput = ''
+    AlternativeOutput = binary(result, AlternativeOutput)
+    print(AlternativeOutput, file=open("output.txt", "a"))
+else:
+    print(result, file=open("output.txt", "a"))
