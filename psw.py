eae2139c49603e133c4cdaac57b6cde688ad3e9d
AlessandroPortatile <alessandro99-c@live.com>
2020-10-12 08:51:05 +0200

    Psw now has to ability to have different lengths of passwords

--- psw.py
+++ psw.py
@@ -1,8 +1,14 @@
 # Define a random password generator. The password should contain 10 characters. Type of characters: alphanumeric
 def psw():
     import random
-    x = '0000000000'
+    p = int(input("password size "))
+    x = ''
     string = ''
+
+    # this cycle allows me to define the length of the password, for a more usable program
+    while p:
+        x = x + '0'
+        p -= 1
 
     for z in x:
         string = string + chr(random.randint(48, 90))
